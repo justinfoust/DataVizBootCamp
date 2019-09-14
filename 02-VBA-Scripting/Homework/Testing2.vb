3572b544066123180e7c62885992da8e56dc513b
justinfoust <54191450+justinfoust@users.noreply.github.com>
2019-09-14 08:25:25 -0700

    none

---
+++ 02-VBA-Scripting/Homework/Testing2.vb
@@ -0,0 +1,28 @@
+Sub stockFormat()
+
+    Dim counter As Single
+    Dim resulteRow As Single
+    
+    Dim iStockValue As Single
+    Dim fStockValue As Single
+    Dim chgStockValue As Single
+    Dim pctChgStockValue As Single
+
+    Dim iVolumeRow As Single
+    Dim fVolumeRow As Single
+    Dim sumVolume As Single
+
+    Dim wsCount As Integer
+    Dim wsNum As Integer
+
+
+    wsCount = ActiveWorkbook.Worksheets.Count
+
+    For wsNum = 1 To wsCount
+
+        
+
+    Next wsNum
+    
+
+End Sub
