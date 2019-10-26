aaa2aa489005e2a8c45384ebae1e00733de590de
justinfoust <54191450+justinfoust@users.noreply.github.com>
2019-10-26 08:36:16 -0700

    none

---
+++ 06-Python-APIs/Day 3/Activities/02-Ins_Google_Places/Solved/config.py
@@ -0,0 +1,2 @@
+# Enter your API key
+gkey = "AIzaSyCol4u7SeaZtyQ_vlqRTkIU2VbZpELQ0-s"
---
+++ Project 1/api_keys.py
@@ -0,0 +1,8 @@
+# Enter your API key
+gkey = "AIzaSyCol4u7SeaZtyQ_vlqRTkIU2VbZpELQ0-s"
+
+yelp_key = "HhJlEV3eWb6Ac5fGJZzX0OQphNVqlZ7B1KsFDM_thrPapyBi6PPVCXFgVj4a0Ow-t6iuzGgqN9Gf8_z-sEVMlin-CNhOXig-Kb3JgaztZ80S0bFBKu-p-zmo7XWyXXYx"
+
+yelp_id = "UwK4d0VgYEeAcTBulv8qpA"
+
+
