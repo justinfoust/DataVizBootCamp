aa08a8653e60b49ca98fb7288ac43e735bf1e008
justinfoust <54191450+justinfoust@users.noreply.github.com>
2019-11-09 09:02:49 -0800

    none

--- 09-SQL/Homework/EmployeeSQL_Schema.sql
+++ 09-SQL/Homework/EmployeeSQL_Schema.sql
@@ -1,70 +1,73 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-
-CREATE TABLE "Depts" (
-    "dept_no" var   NOT NULL,
-    "dept_name" var   NOT NULL,
-    CONSTRAINT "pk_Depts" PRIMARY KEY (
-        "dept_no"
-     )
+CREATE TABLE Depts
+(
+    dept_no VARCHAR PRIMARY KEY,
+    dept_name VARCHAR NOT NULL
 );
 
-CREATE TABLE "Dept_emp" (
-    "emp_no" int   NOT NULL,
-    "dept_no" var   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
+CREATE TABLE Dept_emp 
+(
+    emp_no INT NOT NULL,
+    dept_no VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL
 );
 
-CREATE TABLE "Dept_mgr" (
-    "dep_no" var   NOT NULL,
-    "emp_no" int   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
+CREATE TABLE Dept_mgr 
+(
+    dep_no VARCHAR NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL
 );
 
-CREATE TABLE "emp" (
-    "emp_no" int   NOT NULL,
-    "birth_date" date   NOT NULL,
-    "first_name" var   NOT NULL,
-    "last_name" var   NOT NULL,
-    "gender" var   NOT NULL,
-    "hire_date" date   NOT NULL,
-    CONSTRAINT "pk_emp" PRIMARY KEY (
-        "emp_no"
-     )
+CREATE TABLE Emp 
+(
+    emp_no INT PRIMARY KEY,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    gender VARCHAR NOT NULL,
+    hire_date DATE NOT NULL
 );
 
-CREATE TABLE "sal" (
-    "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
+CREATE TABLE Sal 
+(
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL
 );
 
-CREATE TABLE "titl" (
-    "emp_no" into   NOT NULL,
-    "title" var   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL
+CREATE TABLE Title 
+(
+    emp_no INT NOT NULL,
+    title VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL
 );
 
-ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "emp" ("emp_no");
+ALTER TABLE Dept_emp 
+ADD CONSTRAINT fk_Dept_emp_emp_no 
+FOREIGN KEY(emp_no) REFERENCES Emp (emp_no);
 
-ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Depts" ("dept_no");
+ALTER TABLE Dept_emp
+ADD CONSTRAINT fk_Dept_emp_dept_no
+FOREIGN KEY(dept_no) REFERENCES Depts (dept_no);
 
-ALTER TABLE "Dept_mgr" ADD CONSTRAINT "fk_Dept_mgr_dep_no" FOREIGN KEY("dep_no")
-REFERENCES "Depts" ("dept_no");
+ALTER TABLE Dept_mgr
+ADD CONSTRAINT fk_Dept_mgr_dep_no
+FOREIGN KEY(dep_no) REFERENCES Depts (dept_no);
 
-ALTER TABLE "Dept_mgr" ADD CONSTRAINT "fk_Dept_mgr_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "emp" ("emp_no");
+ALTER TABLE Dept_mgr
+ADD CONSTRAINT fk_Dept_mgr_emp_no
+FOREIGN KEY(emp_no) REFERENCES Emp (emp_no);
 
-ALTER TABLE "sal" ADD CONSTRAINT "fk_sal_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "emp" ("emp_no");
+ALTER TABLE Sal
+ADD CONSTRAINT fk_Sal_emp_no
+FOREIGN KEY(emp_no) REFERENCES Emp (emp_no);
 
-ALTER TABLE "titl" ADD CONSTRAINT "fk_titl_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "emp" ("emp_no");
+ALTER TABLE Title
+ADD CONSTRAINT fk_Title_emp_no
+FOREIGN KEY(emp_no) REFERENCES Emp (emp_no);
 
