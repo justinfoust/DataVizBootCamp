1b3bb08a3bf931e6685e6364d71b428852632331
justinfoust <54191450+justinfoust@users.noreply.github.com>
2020-01-11 14:01:26 -0800

    none

--- 16-D3/Day 1/03-Stu_D3_Table/Unsolved/table.js
+++ 16-D3/Day 1/03-Stu_D3_Table/Unsolved/table.js
@@ -25,3 +25,11 @@ var austinWeather = [{
 }
 ];
 // YOUR CODE HERE
+
+d3.select("tbody").selectAll("tr")
+.data(austinWeather)
+.enter()
+.append("tr")
+.html(function (d) {
+    return `<td>${d.date}</td><td>${d.low}</td><td>${d.high}</td>`
+});
